0c100900858a296ae96997741d605fa2b918defa
Keseru Szabolcs <keserusz94@gmail.com>
2019-10-02 07:27:26 +0300

    Initial commit

---
+++ src/main/java/org/FasttrackIT/App.java
@@ -0,0 +1,13 @@
+package org.FasttrackIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
